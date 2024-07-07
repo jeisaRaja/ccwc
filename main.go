42d37ca5ee37e9db0cf5b52da922b9c937b548dd
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-07-07 19:15:30 +0700

    feat: read from stdin
    - added count words
    - added count characters

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strings"
+	"unicode/utf8"
 )
 
 type CCWC struct {
@@ -23,16 +25,27 @@ func check(e error) {
 }
 
 func main() {
-	var outputBytes = flag.Bool("c", false, "to output the number of bytes")
-	var outputLines = flag.Bool("l", false, "to output the number of bytes")
+	var outputBytes = flag.Bool("c", false, "output the number of bytes")
+	var outputLines = flag.Bool("l", false, "output the number of lines")
+	var outputWords = flag.Bool("w", false, "output the number of words")
+	var outputCharacters = flag.Bool("m", false, "output the number of characters")
 	var fileSource = flag.String("f", "", "locate the file source")
 
 	flag.Parse()
 
-	dat, err := os.ReadFile(*fileSource)
-	check(err)
+	var input []byte
 
-	ccwc := New(dat)
+	if *fileSource != "" {
+		dat, err := os.ReadFile(*fileSource)
+		check(err)
+		input = dat
+	} else {
+		dat, err := io.ReadAll(os.Stdin)
+		check(err)
+		input = dat
+	}
+
+	ccwc := New(input)
 
 	if *outputBytes {
 		ccwc.countBytes()
@@ -40,11 +53,23 @@ func main() {
 	if *outputLines {
 		ccwc.countLines()
 	}
+	if *outputWords {
+		ccwc.countWords()
+	}
+	if *outputCharacters {
+		ccwc.countCharacters()
+	}
+
+	if !*outputBytes && !*outputLines && !*outputWords && !*outputCharacters {
+		ccwc.countLines()
+		ccwc.countWords()
+		ccwc.countBytes()
+	}
 
 	for _, number := range ccwc.FinalOutput {
 		fmt.Printf("%d  ", number)
-		fmt.Println("")
 	}
+	fmt.Println("")
 }
 
 func (wc *CCWC) countBytes() {
@@ -57,6 +82,11 @@ func (wc *CCWC) countLines() {
 }
 
 func (wc *CCWC) countWords() {
-	words := strings.Split(string(wc.Input), " ")
-	wc.FinalOutput = append(wc.FinalOutput, len(words)-1)
+	words := strings.Fields(string(wc.Input))
+	wc.FinalOutput = append(wc.FinalOutput, len(words))
+}
+
+func (wc *CCWC) countCharacters() {
+	lenCharacters := utf8.RuneCountInString(string(wc.Input))
+	wc.FinalOutput = append(wc.FinalOutput, lenCharacters)
 }
