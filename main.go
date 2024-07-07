5ae40b4317fafb838371860295a9c90c5f04ac61
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-07-07 18:29:29 +0700

    add count lines

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type CCWC struct {
@@ -22,8 +23,9 @@ func check(e error) {
 }
 
 func main() {
-	var outputBytes = flag.Bool("bytes", false, "to output the number of bytes")
-	var fileSource = flag.String("file", "", "locate the file source")
+	var outputBytes = flag.Bool("c", false, "to output the number of bytes")
+	var outputLines = flag.Bool("l", false, "to output the number of bytes")
+	var fileSource = flag.String("f", "", "locate the file source")
 
 	flag.Parse()
 
@@ -35,13 +37,26 @@ func main() {
 	if *outputBytes {
 		ccwc.countBytes()
 	}
+	if *outputLines {
+		ccwc.countLines()
+	}
 
 	for _, number := range ccwc.FinalOutput {
 		fmt.Printf("%d  ", number)
-    fmt.Println("")
+		fmt.Println("")
 	}
 }
 
 func (wc *CCWC) countBytes() {
 	wc.FinalOutput = append(wc.FinalOutput, len(wc.Input))
+}
+
+func (wc *CCWC) countLines() {
+	lines := strings.Split(string(wc.Input), "\n")
+	wc.FinalOutput = append(wc.FinalOutput, len(lines)-1)
+}
+
+func (wc *CCWC) countWords() {
+	words := strings.Split(string(wc.Input), " ")
+	wc.FinalOutput = append(wc.FinalOutput, len(words)-1)
 }
